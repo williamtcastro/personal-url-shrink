391aca5f8d886ea36d27308414d428a08f6cd4d4
William de Castro <williamtdcastro@gmail.com>
2024-07-13 23:57:26 -0300

    chore: add new redirect

---
+++ src/app/[slug]/route.ts
@@ -0,0 +1,7 @@
+export async function GET(
+  request: Request,
+  { params }: { params: { slug: string } }
+) {
+  const slug = params.slug; // 'a', 'b', or 'c'
+  return Response.json({ slug });
+}
